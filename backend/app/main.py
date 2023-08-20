f56b8e674fa1442e7c1a52eadff868066c1274bc
Akotet08 <akiyeshaw@gmail.com>
2023-08-20 11:51:52 +0900

    auth done

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from starlette.middleware.cors import CORSMiddleware
 
-from app.api.endpoints import router
-
+from app.api.endpoints import router as endpoint_router
+from app.api.auth import router as auth_router
+# from app.get_model import get_model
 
 def init_app():
     app = FastAPI()
@@ -15,11 +16,13 @@ def init_app():
         allow_headers=["*"],
     )
 
-    app.include_router(router)
+    app.include_router(endpoint_router)
+    app.include_router(auth_router)
 
     return app
 
 app = init_app()
+
 
 @app.get("/")
 def root():
